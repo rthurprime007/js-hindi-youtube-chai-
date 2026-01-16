8fe083e7270f6c31eaf0dccdab8f628860be5676
rthurprime007 <rthurprime@gmail.com>
2026-01-16 13:34:36 +0000

    save it

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 123321
+let accountEmail = "win@gmail.com"
+var accountPassword = "121212"
+accountCity = "meerut"
+
+//accountId = 54
+
+accountEmail = "jav@gmail"
+accountPassword = "1313"
+accountCity = "pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
