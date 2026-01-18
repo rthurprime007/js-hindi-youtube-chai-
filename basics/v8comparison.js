b8a26703e35976d23412b34474ee84e4e96fa959
rthurprime007 <rthurprime@gmail.com>
2026-01-18 07:42:31 +0000

    datatypesummary

---
+++ basics/v8comparison.js
@@ -0,0 +1,30 @@
+// comparison answer will be in boolean true or false
+//console.log(2>1);
+//console.log(2<1)
+//console.log(2 >= 1) more than equal to
+//console.log(2==1) is equals to
+//console.log(2!=1) not equal to
+
+//same goes for strings but what will happen if there are different data types
+
+
+
+//console.log(null>0)      false 
+//console.log(null == 0)   false
+//console.log(null >= 0)   true (the reason is that an equality check == and comparisons > , < , >= , <= works differentley)
+//omparisons convert null to a number , treating it a 0 thats why null >=0 is true while null >0 is false
+
+//console.log(undefined == 0) false
+//console.log(undefined > 0)   false
+//console.log(undefined < 0)  false
+
+//(from line 12 to 19 these are very confusing so revise )
+
+
+
+//***strict check ******
+// (===) = strict check
+
+console.log("2" == 2)   // == only check numbers
+console.log("2" === 2)  // === check numbers with datatype
+
---
+++ basics/v9datatypessummary.js
@@ -0,0 +1,55 @@
+// #Primitive 
+// 7 types
+//1. string
+//2.Number
+//3.Boolean
+//4.null
+//5.undefined
+//6.Symbol (to give a value uniqueness)
+//7.BigInt
+
+//************FOR CHECKING OUT SYMBOL DATATYPE**** */
+const id = Symbol('123')
+const anotherid = Symbol('123')
+//their value maybe same but they are still different 
+console.log(id === anotherid)
+//it will show false
+
+
+
+// Refrence type (non primitive)
+// 1. Array
+// 2. Objects
+// 3. Functions
+
+// arrays ko hum [] main likhte ha 
+//**********example of an array */
+const heroes = ["shaktiman" , "naagraj" , "doga"]
+
+//Objects ko hum {} main likhte haan 
+//*************example of an object********** */
+let myObj = {
+    name: "java",
+    age: 27     
+}
+
+
+//*****************functions*************
+funstion ko declare krne k kafi tarike ha yaha hum variable wala tarika seekhenge
+//function(){}  this is function
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+
+//non primitive ka console.log(typeof) hamesha function hi ayega
+
+//to study
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//q. IS JAVA CODING LANGUAGE DYNAMIC OR STATISTICALLY TYPED ?
+
+
+
+
