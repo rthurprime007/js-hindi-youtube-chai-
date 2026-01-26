ba1e4919606b17cd4ceb8d8bad2a49307f616757
rthurprime007 <rthurprime@gmail.com>
2026-01-26 07:57:29 +0000

    ARRAY HALF UNDERSTOOD

--- 2basics/practise.js
+++ 2basics/practise.js
@@ -1,6 +1,17 @@
-const myarr = [1 , 2 ,3 ,4]
+const myarr = ["javad", "ahmed", "arthur" , " prime"] 
 
-let newarr = myarr.splice(2 , 1 , 9)
+//EXAMPLE = SLICE
+const A = myarr.slice(1 , 3)  //SLICE LOGIC slice (start[will be included , end[will nnot be included]]
+console.log("a:", A);                  //RESULT > a: [ 'ahmed', 'arthur' ]
+console.log("a:", myarr.slice(0 , 3)); // RESULT > a: [ 'javad', 'ahmed', 'arthur' ] ISME CONTANT NAHI LIA
+ 
+//EXAMPLE = SPLICE  //SPLICE 
+//IF YOU WANT TO RETURN A DELETED ELEMENT IN ARRAY SPLICE(INDEX , DELETE)
+//IF YOU WANT TO MODIFY AN ELEMENT IN ARRAY  SPLICE (INDEX , DELETE ,MODIFY)
+//if introduced a const than it will return the value 
 
-console.log(newarr);
-
+const B = myarr.splice(2,1) > RESULT> [ 'arthur' ]
+const B = myarr.splice(2 , 0 , "RANI") //RESULT >b: [ 'javad', 'ahmed', 'RANI', 'arthur', ' prime' ]
+const B = myarr.splice(2 , 1 , "RANI") //RESULT >b: [ 'javad', 'ahmed', 'RANI', ' prime' ]
+const B = myarr.splice(2 , 0 , "RANI", "LAXMI") //RESULT>b: [ 'javad', 'ahmed', 'RANI', 'LAXMI', 'arthur', ' prime' ]
+myarr.SPLICE() // THIS IS ALSO A METHOD
