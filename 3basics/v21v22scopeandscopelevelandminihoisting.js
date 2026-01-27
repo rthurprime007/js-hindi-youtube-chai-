dc432329cd633ebdfbed6daa8069f55f4e951e3f
rthurprime007 <rthurprime@gmail.com>
2026-01-27 06:29:58 +0000

    v22

---
+++ 3basics/v21v22scopeandscopelevelandminihoisting.js
@@ -0,0 +1,147 @@
+let a = 10 
+const b = 20
+var c = 30
+//teeno k answer 10 , 20 , 30 hi ayenge 
+//to phir problem kaha ayi
+//to problem kaha ayi var k sath
+
+//{} < isko kehte ha scope    if ke sath use krenge aaj
+
+if (true) {
+    let d = 50
+    const e = 60
+    var f = 70
+}
+
+
+    let d = 500
+    const e = 600
+    var f = 700
+//console.log(d); result > error
+//console.log(e); result > error 
+//console.log(f); result > 700
+//////////value kahi bhi change kr skte ha var ki
+// ek problem aur ha var k sathn scope ke andar cheeze andar ki ha 
+//aur bahar ki cheeze bhar ki ha 
+//ye execute nahi hone chahiye 
+
+
+
+//  if (true) {
+//       d = 50
+//  }
+// d = 70000
+// console.log(d); result > 7000 ese bhi value change ho jati ha 
+
+let g = 150
+if (true) {
+    let g = 250
+    console.log("inner:", g)   //result > inner: 250   block scope main (g) ki value
+}
+
+//ek baat aur scope k annadr ki cheeze block scope hoti ha
+//aur scope k bahar ki cheeze global scope
+console.log(g); // result > 150  global scope main (g ) ki value
+
+
+//browser k console pr scope alag ha aur node main scope alag ha 
+
+//*******************************v22 scopelevel and mini hoisting***************
+//remember chote bache bado se icecream le skte ha lekin
+//lekin bade choto se nahi le skyte
+
+// function one(){
+//     const username = "javad"
+//     function two() {
+//         const website = "youtube"
+//         console.log(username);
+        
+//     }
+//     console.log(website);
+//  two()    
+// }
+// one()//erro dega kyoki console.log(website) andar wale function ki value ha
+// //aur vahi khatam ho jani chahiye 
+
+
+
+
+// function one(){
+//     const username = "javad"
+//     function two() {
+//         const website = "youtube"
+//         console.log(username);
+        
+//     }
+ 
+// }
+// one() // ab execute hoga hi nahi kyoki anadar wala execute hi nai hua
+
+function one(){
+   const username = "javad"
+     function two() {
+         const website = "youtube"
+         console.log(username);
+        
+     }
+     
+  two()    
+ }
+ one() // result > javad 
+
+
+
+//for if */
+// if (true) {
+//      const username = "javad"
+//      if(username==="javad"){
+//         const website = " youtube"
+//         console.log(username + website)
+//      }
+//      console.log(website);   //pehla error idhar ayega kyoki ye anadr ki value ha
+     
+//  }
+
+//  console.log(username)
+
+
+//  if (true) {
+//      const username = "javad"
+//      if(username==="javad"){
+//         const website = " youtube"
+//         console.log(username + website)
+//      }
+     
+     
+//  }
+
+//  console.log(username) ab yaha error dikayega kykoi ye bhi andar hi execute hona ha
+
+
+
+ if (true) {
+     const username = "javad"
+     if(username==="javad"){
+        const website = " youtube"
+        console.log(username + website)
+     }
+     
+     
+ }  //result = javad youtube
+
+ //++++++++++++ interesting ++++++++++++//
+
+ addone(5) 
+ function addone(num){
+    return num +1
+ }                        //agar ese likhege to anser to nahi ayega lekin koi error 
+ //bhi nahi ayegi
+
+  
+
+ addtwo(5)
+ const addtwo = function(num){
+    return num + 2
+ }     //ab ye error dikhaye jab const k sth function use kia humne
+
+ //yahi interesting cheez ha
---
+++ 3basics/v22arrow.js
@@ -0,0 +1,141 @@
+const user ={
+    username : "javad",
+    age : 27,
+
+    welcomemessage:function(){
+        //console.log(`${username} , welocome to website`); isme direct mian username nahi kr skta 
+        console.log(`${this.username} , welocome to website`); //ye this lagana padega
+                                                               // ye ab current context to use krega
+        
+    }
+}
+user.welcomemessage()  //result > javad , welocome to website
+user.username = "sam"
+user.welcomemessage()  //result > sam , welocome to website
+
+
+
+/////////////////ab this ka concept padenge
+const newuser ={
+    username : "javad",
+    age : 27,
+
+    welcomemessage:function(){
+        
+        console.log(`${this.username} , welocome to website`); 
+        console.log(this); 
+        
+    }
+}
+
+newuser.welcomemessage()  
+newuser.username = "sam"  //jab teno ko print kia result>   {
+ newuser.welcomemessage()  
+//                                                             username: 'javad',
+//                                                              age: 27,
+//                                                             welcomemessage: [Function: welcomemessage]
+//                                                              }
+//                                                              sam , welocome to website
+//                                                              {
+//                                                               username: 'sam',
+ //                                                               age: 27,
+ //                                                              welcomemessage: [Function: welcomemessage]
+//                                                                }
+
+//                                                                newuser.welcomemessage()  
+
+//////explation   iska matlab (THIS)  current context ki baat krta ha
+
+
+console.log(this); //result> {}   agar ese hi print kr denge bahae kahi to ye empty dikhayga
+                                  // iska matlab koi current context ha hi nahi
+                                  // ye  node jo ham kr rahe ha yaha ye empty dikhayega
+                                  // console pr yeh empty window object rahega //interview ques
+                                  //  waha pr hum iske events bhi capture kr skte ha
+
+ function chai (){
+    console.log(this);
+    
+ }                            
+ chai()      //result > pta na kya kya aagya
+//  <ref *1> Object [global] {
+//   global: [Circular *1],
+//   clearImmediate: [Function: clearImmediate],
+//   setImmediate: [Function: setImmediate] {
+//     Symbol(nodejs.util.promisify.custom): [Getter]
+//   },
+//   clearInterval: [Function: clearInterval],
+//   clearTimeout: [Function: clearTimeout],
+//   setInterval: [Function: setInterval],
+//   setTimeout: [Function: setTimeout] {
+//     Symbol(nodejs.util.promisify.custom): [Getter]
+//   },
+//   queueMicrotask: [Function: queueMicrotask],
+//   structuredClone: [Function: structuredClone],
+//   atob: [Function: atob],
+//   btoa: [Function: btoa],
+//   performance: [Getter/Setter],
+//   fetch: [Function: fetch],
+//   crypto: [Getter],
+//   navigator: [Getter]
+// }
+
+
+function doosrichai() {
+    let username = "javad"
+    console.log(this.username)
+} 
+doosrichai() //result > undefined  sir ka kehna ha ye jese object main kaam krega vese function 
+                            // main kaam nahi krega
+                            // ek experiment krio function mian object introduce krkr dekhiyo
+// agar doosri tarah se function ko declare kre
+const teesrichai = function(){
+        let username = "javad"
+        console.log(this.username);
+        
+}
+
+teesrichai() //result = undefined    ese bhi answer same raha
+//arrow function ese bnta ha isko bhi try krenge
+
+const chotichai = () => {   //isko kehte ha arrow function
+        let username = "javad"
+        console.log(this.username);
+}
+chotichai()  //result = undefined
+
+/////////////////ab krenge core arrow function
+//() => {}      is ko kehte ha arrow function 
+const addtwo = (num1 , num2) => {
+    return num1 + num2
+}
+
+addtwo(3,4)// lekin ye ese call nahi hua
+console.log(addtwo(3,4)); //result > 7
+
+///////////////////implicit return    is methos main curly braces use nahi hote ha
+
+const addthree = (num1 , num2 , num3) =>  num1 + num2 + num3
+console.log(addthree(1,2,3)); //result> 6
+const mixthree = (num1 , num2 , num3) => ( num1 + num2 + num3 )
+console.log(addthree(1,2,3)); //result> 6 parnthese main pack krdo ( num1 + num2 + num3 ) same result
+  
+
+//curly braces main    return key likhna hi padega
+//parenthesis main return key nahi lagana padega
+
+//reactjs main bohot kaam ayega
+const experiment = (num1 , num2) => {usernma: "javad"}  //call result > undefined
+const experiment2 = (num1 , num2) => ({usernma: "javad"}) //call result> javad
+ ///iska matlab agar retuurn nahi dena to parenthesis dena hi padega
+
+
+//looping ka ek examole ye samajne k lie ki looping main kese kaam ayega
+const myarray = [1 , 2 , 3 , 4 ,5]
+myarray.forEach(() => {})
+
+
+
+/////////////
+
+
